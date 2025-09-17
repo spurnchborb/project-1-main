45f643b272fd4dbb7edd045c29967089d6981c3f
spurnchborb <biagathms@gmail.com>
2025-09-17 08:56:00 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let currentDate = new Date().toLocaleDateString();
+let date = document.querySelector("#date");
+
+date.textContent = currentDate;
+let currentYear = new Date().getFullYear();
+let year = document.querySelector("#year");
+
+year.textContent = currentYear;
